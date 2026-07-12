d0360f90f0c434e77ca9969010b46f619a248b34
neomi5318-do <neomi.n5318@gmail.com>
2026-07-13 01:41:55 +0300

    update recipe modal details

--- server/app/routes/recipes_route.py
+++ server/app/routes/recipes_route.py
@@ -1,11 +1,45 @@
+# from fastapi import APIRouter
+# from typing import List  # בשביל הפרוטוקול, מייבאים List עם L גדולה מ-typing
+# from app.controllers.recipes_controller import get_recommended_recipes
+
+# router = APIRouter()
+
+# @router.post("/recommend")
+# async def recommend_recipes(ingredients: list[str]):
+#     # קריאה ל-Controller שיבצע את הפנייה למאגר ויחשב את הנתונים
+#     result = get_recommended_recipes(ingredients)
+#     return result
+
+
+#     # חדשששששששששששששש
+#     from app.controllers.recipes_controller import get_recommended_recipes
+# from app.database import fetch_recipe_details # נוסיף את הייבוא הזה
+
+# @router.get("/{recipe_id}/details")
+# async def get_recipe_details(recipe_id: int):
+#     details = fetch_recipe_details(recipe_id)
+#     if details:
+#         return {"success": True, "recipe": details}
+#     return {"success": False, "message": "Recipe not found"}
+
+
+
 from fastapi import APIRouter
-from typing import List  # בשביל הפרוטוקול, מייבאים List עם L גדולה מ-typing
 from app.controllers.recipes_controller import get_recommended_recipes
+from app.database import fetch_recipe_details
 
 router = APIRouter()
 
 @router.post("/recommend")
 async def recommend_recipes(ingredients: list[str]):
-    # קריאה ל-Controller שיבצע את הפנייה למאגר ויחשב את הנתונים
+    # קריאה ל-Controller
     result = get_recommended_recipes(ingredients)
     return result
+
+@router.get("/{recipe_id}/details")
+async def get_recipe_details(recipe_id: int):
+    # שליפת פרטי מתכון בודד
+    details = fetch_recipe_details(recipe_id)
+    if details:
+        return {"success": True, "recipe": details}
+    return {"success": False, "message": "Recipe not found"}
